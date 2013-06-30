fe7ad6c11a71f26b507d89f5597633fcf111cddd
Erik Olsson <meriko@hotmail.com>
2013-06-30 14:12:13 +0200

    First draft for updated website

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,108 +1,53 @@
-NAME = 'Comedians in Cars Getting Coffee'
-ART = 'art-default.jpg'
+TITLE = 'Comedians In Cars Getting Coffee'
+
+ART  = 'art-default.jpg'
 ICON = 'icon-default.png'
 
-BASE_URL = 'http://www.comediansincarsgettingcoffee.com'
-ASSET_URL = 'http://assets.comediansincarsgettingcoffee.com/%s/%s-%s-%s'
+BASE_URL = 'http://comediansincarsgettingcoffee.com'
 
 ###################################################################################################
+
 def Start():
+	Plugin.AddPrefixHandler('/video/cicgc', MainMenu, TITLE, ICON, ART)
+	Plugin.AddViewGroup('List', viewMode = 'List', mediaType = 'items')
 
-	ObjectContainer.art = R(ART)
-	ObjectContainer.title1 = NAME
+	# Set the default ObjectContainer attributes
+	ObjectContainer.title1     = TITLE
+	ObjectContainer.view_group = 'List'
+	ObjectContainer.art        = R(ART)
 
+	# Default icons for DirectoryObject and VideoClipObject in case there isn't an image
 	DirectoryObject.thumb = R(ICON)
-	DirectoryObject.art = R(ART)
+	DirectoryObject.art   = R(ART)
 	VideoClipObject.thumb = R(ICON)
+	VideoClipObject.art   = R(ART)
 
+	# Set the default cache time
 	HTTP.CacheTime = CACHE_1HOUR
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:18.0) Gecko/20100101 Firefox/18.0'
 
 ###################################################################################################
-@handler('/video/cicgc', NAME, thumb=ICON, art=ART)
 def MainMenu():
-
-	# Constructs the main episode listing.	Since each episode has the main episode
-	# plus bonus content, make each one a separate directory
 	oc = ObjectContainer()
+	
+	pageElement = HTML.ElementFromURL(BASE_URL)
+	
+	# Add shows by parsing the site
+	for item in pageElement.xpath("//*[contains(@class, 'all-episodes')]//li"):
+		video = {}
 
-	page = HTML.ElementFromURL(BASE_URL)
+		video["url"]  = BASE_URL + item.xpath(".//a/@href")[0]
+		video["img"]  = item.xpath(".//img/@src")[0]
+		video["name"] = item.xpath(".//h2//span/text()")[0]
+		video["desc"] = item.xpath(".//p/text()")[0].strip()
 
-	# All the episode info is passed as a dictionary to a javascript function --
-	# let that do all the work for us...
-
-	# The script is javascript and has no source
-	scripts = page.xpath('''//script[@type='text/javascript' and not(@src)]/text()''')
-
-	for s in scripts:
-		# The one we want runs on $(document).ready
-		if(s.find("$(document).ready(function()") != -1):
-			# Use the JSON parser to get python objects
-			eplist = JSON.ObjectFromString(s[s.find("["):s.rfind("]")+1])
-
-			# Purely for cosmetic reasons, I want the preview to be the first item in the list
-			# However, it is listed last, so parse the list twice, looking for the preview first...
-			for ep in eplist:
-				if ep['type'] == 'preview':
-					title = 'Coming Soon: ' + ep['guest']
-					summary = ep['title']
-					url = '/'.join([BASE_URL, ep['slug']])
-					thumb = ASSET_URL % (ep['slug'], ep['type'], ep['key'], 'poster.jpg')
-
-					# Get duration in milliseconds
-					dur = ep['videos'][0]['duration'].split(':')
-					duration = (int(dur[0])*60 + int(dur[1])) * 1000
-
-					oc.add(VideoClipObject(
-						url = url,
-						title = title,
-						thumb = thumb,
-						summary = summary,
-						duration = duration
-					))
-
-			# Now get the full episodes
-			for ep in eplist:
-				if ep['type'] == 'full':
-					title = 'Ep. ' + str(int(ep['id']) // 1000) + ': ' + ep['guest']
-					summary = ep['title']
-					url = '/'.join([BASE_URL, ep['slug']])
-					thumb = ASSET_URL % (ep['slug'], ep['video_slug'], ep['key'], 'poster.jpg')
-
-					oc.add(DirectoryObject(
-						key = Callback(EpisodeMenu, ep=ep, title=title),
-						title = title,
-						thumb = thumb,
-						summary = summary
-					))
-
+		oc.add(
+			EpisodeObject(
+				url = video["url"],
+				title = video["name"],
+				show = TITLE,
+				summary = video["desc"],
+				thumb = video["img"])
+		)
+			 
 	return oc
 
-####################################################################################################
-@route('/video/cicgc/episodes', ep=dict)
-def EpisodeMenu(ep, title):
-
-	# Constructs the menu for a single episode (episode + bonus content)
-	oc = ObjectContainer(title2=title)
-
-	for vid in ep['videos']:
-		if vid['title'] == '':
-			title = ep['title']
-		else:
-			title = vid['title']
-
-		url = '/'.join([BASE_URL, ep['slug'], vid['slug']])
-		thumb = ASSET_URL % (ep['slug'], vid['slug'], ep['key'], 'poster.jpg')
-
-		# Get duration in milliseconds
-		dur = vid['duration'].split(':')
-		duration = (int(dur[0])*60 + int(dur[1])) * 1000
-
-		oc.add(VideoClipObject(
-			url = url,
-			title = title,
-			thumb = thumb,
-			duration = duration
-		))
-
-	return oc
